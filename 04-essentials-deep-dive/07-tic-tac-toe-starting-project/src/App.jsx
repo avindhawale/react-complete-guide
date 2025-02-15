0e6ba1427c08288f610cbbb130affd2896ff3c64
avindhawale <avin.dhawale@gmail.com>
2025-02-16 00:13:36 +0530

    04-essentials-deep-dive : 07-tic-tac-toe-starting-project : deriving computed values from other computed values

--- 04-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -34,6 +34,24 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  let winner;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((currActivePlayer) =>
     //   currActivePlayer === Player1_Symbol ? Player2_Symbol : Player1_Symbol
@@ -65,6 +83,7 @@ function App() {
             isActive={activePlayer === Player2_Symbol}
           />
         </ol>
+        {winner && <p>You won, {winner}!</p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
