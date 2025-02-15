235978f04be5e36debc172052a84351979377b05
avindhawale <avin.dhawale@gmail.com>
2025-02-15 23:59:02 +0530

    04-essentials-deep-dive : 07-tic-tac-toe-starting-project : lifting computed values up

--- 04-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import Player from "./components/Player.jsx";
-import WINNING_COMBINATIONS from "./winning-combinations.js";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
 const Player1_Symbol = "X";
 const Player2_Symbol = "O";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function derivedActivePlayer(gameTurns) {
   let currentPlayer = Player1_Symbol;
@@ -19,6 +25,14 @@ function App() {
   //const [activePlayer, setActivePlayer] = useState(Player1_Symbol);
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = derivedActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((currActivePlayer) =>
@@ -51,7 +65,7 @@ function App() {
             isActive={activePlayer === Player2_Symbol}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
