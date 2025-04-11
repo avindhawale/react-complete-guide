bae44cf02e9000bb08ede225a281b3e52c51c9e7
avindhawale <avin.dhawale@gmail.com>
2025-04-11 17:08:48 +0530

    08-refs-portals : 01-starting-project : portals

--- 08-refs-portals/01-starting-project/src/components/ResultModal.jsx
+++ 08-refs-portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from "react"
+import { createPortal } from 'react-dom';
 
 export default function ResultModal({remainingTime, targetTime, onReset, ref}){
 
@@ -15,18 +16,21 @@ export default function ResultModal({remainingTime, targetTime, onReset, ref}){
         }
     });
 
-    return <dialog ref={dialog} className="result-modal">
-        { userLost && <h2>You lost</h2> }
-        { !userLost && <h2>You score : {score}</h2> }
-        <p>
-            The target time was <strong>{targetTime} seconds.</strong>
-        </p>
-        <p>
-            You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
-        </p>
+    return createPortal(
+        <dialog ref={dialog} className="result-modal">
+            { userLost && <h2>You lost</h2> }
+            { !userLost && <h2>You score : {score}</h2> }
+            <p>
+                The target time was <strong>{targetTime} seconds.</strong>
+            </p>
+            <p>
+                You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
+            </p>
 
-        <form method="dialog" onSubmit={onReset}>
-            <button>Close</button>
-        </form>
-    </dialog>
+            <form method="dialog" onSubmit={onReset}>
+                <button>Close</button>
+            </form>
+        </dialog>,
+        document.getElementById('modal')
+    )
 }
