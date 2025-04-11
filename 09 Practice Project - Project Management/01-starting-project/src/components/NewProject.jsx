b94ea5a2951868be4e6452b216dbd5bc29289a28
avindhawale <avin.dhawale@gmail.com>
2025-04-11 19:43:08 +0530

    09 Practice Project - Project Management : 01-starting-project : splitting components for higher reusability

--- 09 Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
+++ 09 Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
@@ -1,6 +1,6 @@
 import Input from "./Input";
 
-export default function NewProject(){
+export default function NewProject() {
     return <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
@@ -8,7 +8,7 @@ export default function NewProject(){
         </menu>
         <div>
             <Input label="Title" />
-            <Input label="Description" textarea/>
+            <Input label="Description" textarea />
             <Input label="Due date" />
         </div>
     </div>
