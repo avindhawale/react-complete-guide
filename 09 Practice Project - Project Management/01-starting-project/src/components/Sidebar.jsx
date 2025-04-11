c9696ec82f1d93707beeee8a2cf3d3636108e5d4
avindhawale <avin.dhawale@gmail.com>
2025-04-11 17:51:24 +0530

    09 Practice Project - Project Management : 01-starting-project : adding sidebar component

---
+++ 09 Practice Project - Project Management/01-starting-project/src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+export default function Sidebar(){
+    return <aside>
+        <h2>Your Projects</h2>
+        <div>
+        <ul></ul>
+        </div>
+        <button>
+            + Add Project
+        </button>
+
+    </aside>
+}
