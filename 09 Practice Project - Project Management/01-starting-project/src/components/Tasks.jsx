4af317dc961fd00d21992b6c5e6427c48c9a1072
avindhawale <avin.dhawale@gmail.com>
2025-04-12 17:41:26 +0530

    09 Practice Project - Project Management : 01-starting-project : adding project tasks component

---
+++ 09 Practice Project - Project Management/01-starting-project/src/components/Tasks.jsx
@@ -0,0 +1,14 @@
+import NewTask from "./NewTask";
+
+export default function Tasks() {
+    return (
+        <section>
+            <h2 className="text-xl font-bold text-stone-700 my-4">Tasks</h2>
+            <NewTask />
+            <p className="text-stone-800 mb-4">
+                This project does not have any tasks yet.
+            </p>
+            <ul></ul>
+        </section>
+    )
+}
