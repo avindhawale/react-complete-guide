8bb53fda076dff359548c5cc8f44e7279ffd5f84
avindhawale <avin.dhawale@gmail.com>
2025-04-16 17:41:45 +0530

    10 State Management with Context useReducer : 01-starting-project :  Migrating the entire demo project to use the context API

--- 10 Advanced State Management with Context useReducer/01-starting-project/src/components/Cart.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/components/Cart.jsx
@@ -1,9 +1,9 @@
 import { use } from 'react';
 import { CartContext } from '../store/shopping-cart-context';
 
-export default function Cart({ onUpdateItemQuantity }) {
+export default function Cart() {
 
-  const { items } = use(CartContext);
+  const { items, updateItemQuantity } = use(CartContext);
 
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -26,11 +26,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
