9524c90d31f3b7df955c48c8230fcc2b3dca416c
avindhawale <avin.dhawale@gmail.com>
2025-04-22 16:08:10 +0530

    12 Demo Project - React Quiz : 01-starting-project : Deriving Values, Outputting Questions & Registering Answers

--- 12 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ 12 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react"
+import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
 
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
 
-    return <p> Currently active question</p>
+    function handelSelectAnswer(selectedAnswer) {
+        setUserAnswers(prevAnswers => [...prevAnswers, selectedAnswer]);
+    }
+
+    return (
+        <div id="quiz">
+            <div id="questions">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                        <li key={answer} className="answer">
+                            <button onClick={() => handelSelectAnswer(answer)}>{answer}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
