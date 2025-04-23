c58135e4117c670340c49e59deab2454b364e4c1
avindhawale <avin.dhawale@gmail.com>
2025-04-23 08:31:54 +0530

    12 Demo Project - React Quiz : 01-starting-project : splitting components up to solve problems

--- 12 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ 12 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -1,16 +1,30 @@
-import { useCallback, useState } from "react"
-import QUESTIONS from "../questions.js";
+import { useCallback, useState } from "react";
 import quizCompletedImg from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer.jsx";
+import QUESTIONS from "../questions.js";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
 
+    const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
 
     const handelSelectAnswer = useCallback(function handelSelectAnswer(selectedAnswer) {
+        setAnswerState('answered');
         setUserAnswers(prevAnswers => [...prevAnswers, selectedAnswer]);
-    }, []);
+
+        setTimeout(() => {
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                setAnswerState('correct');
+            } else {
+                setAnswerState('wrong');
+            }
+
+            setTimeout(() => {
+                setAnswerState('');
+            }, 2000);
+        }, 1000);
+    }, [activeQuestionIndex]);
 
     const handelSkipSelectAnswer = useCallback(() => handelSelectAnswer(null), [handelSelectAnswer]);
 
@@ -24,25 +38,17 @@ export default function Quiz() {
         )
     }
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort((a, b) => Math.random() - 0.5);
-
     return (
         <div id="quiz">
-            <div id="questions">
-                <QuestionTimer
-                    key={activeQuestionIndex}
-                    timeout={10000}
-                    onTimeout={handelSkipSelectAnswer} />
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswers.map(answer => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handelSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <Question
+                key={activeQuestionIndex}
+                questionText={QUESTIONS[activeQuestionIndex].text}
+                answers={QUESTIONS[activeQuestionIndex].answers}
+                answerState={answerState}
+                selectedAnswer={userAnswers[userAnswers.length - 1]}
+                onSelectAnswer={handelSelectAnswer}
+                onSkipAnswer={handelSkipSelectAnswer}
+            />
         </div>
     )
 }
