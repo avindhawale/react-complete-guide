a3058e84559e4f88a92bd9be13f002442961e9b3
avindhawale <avin.dhawale@gmail.com>
2025-05-01 00:44:29 +0530

    15-HTTP-Requests : 01-starting-project : sending data with POST requests

--- 15-HTTP-Requests/01-starting-project/src/http.js
+++ 15-HTTP-Requests/01-starting-project/src/http.js
@@ -7,3 +7,19 @@ export async function fetchAvailablePlaces() {
 
     return data.places;
 }
+
+export async function updateUserPlaces(places) {
+    const response = await fetch("http://localhost:3000/user-places", {
+        method: "PUT",
+        body: JSON.stringify({ places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const resData = response.json();
+    if (!response.ok) {
+        throw new Error("Failed to update the places.")
+    }
+
+    return resData.message;
+}
