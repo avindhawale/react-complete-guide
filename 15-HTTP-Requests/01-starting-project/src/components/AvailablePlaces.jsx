7e595f73990bcc04f3ea43d0376dff5eddeab4f8
avindhawale <avin.dhawale@gmail.com>
2025-05-01 00:04:20 +0530

    15-HTTP-Requests : 01-starting-project : Handling loading states

--- 15-HTTP-Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15-HTTP-Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -4,12 +4,15 @@ import Places from './Places.jsx';
 export default function AvailablePlaces({ onSelectPlace }) {
 
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function getData() {
+      setIsLoading(true);
       const response = await fetch("http://localhost:3000/places");
       const data = await response.json();
       setAvailablePlaces(data.places);
+      setIsLoading(false);
     }
     getData();
 
@@ -20,6 +23,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
       title="Available Places"
       places={availablePlaces}
       fallbackText="No places available."
+      isLoading={isLoading}
       onSelectPlace={onSelectPlace}
     />
   );
