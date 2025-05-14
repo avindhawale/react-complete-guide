7f0e42db7b46cc80d702871c0b112db30cc1d185
avindhawale <avin.dhawale@gmail.com>
2025-05-14 19:23:58 +0530

    19-practice-project-food-order : getting started with Cart Context and Reducer

---
+++ 19-practice-project-food-order/01-starting-project/src/store/CartContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    //check the index of the item if it is already present
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const updatedItems = [...state.items];
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+  }
+}
+
+function CartContextProvider({ children }) {
+  useReducer(cartReducer, { items: [] });
+  return <CartContext>{children}</CartContext>;
+}
+
+export default CartContext;
